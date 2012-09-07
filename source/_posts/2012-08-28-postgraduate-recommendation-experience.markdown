a3db7b0d4c4ff4e67e056575958d4b6c0af10eb2
lgrcyanny <lgrcyanny@gmail.com>
2012-09-07 21:29:48 +0800

    Add code snippets

--- source/_posts/2012-08-28-postgraduate-recommendation-experience.markdown
+++
@@ -1,7 +0,0 @@
----
-layout: post
-title: "Postgraduate Recommendation Experience"
-date: 2012-08-28 21:32
-comments: true
-categories: Life
----
--- source/downloads/code/algorithm/heapsort.java
+++ source/downloads/code/algorithm/heapsort.java
@@ -1,14 +1,18 @@
+/**
+ * Heap sort for an array
+ */
 public class HeapSort
 {
 	public static void heapSort (int[] arr)
 	{
 		int i = 0;
 		// Build a max heap
-		for (i = arr.length / 2; i >= 0 ; i-- )
+		for (i = arr.length / 2; i >= 0 ; i--)
 		{
 			filterDown(arr, i, arr.length);
 		}
-		//Swap the last element with the first element, then bulid the max heap again
+		//Swap the last element with the first element,
+		//then bulid the max heap again
 		for (i = arr.length - 1; i >= 0; i--)
 		{
 			swap(arr, 0, i);
@@ -27,7 +31,6 @@ public class HeapSort
 	}
 
 	/**
-	 *
 	 * @param arr  The array to be heap sorted
 	 * @param i    The index which is the start to be filter down
 	 * @param n    The length
@@ -39,7 +42,8 @@ public class HeapSort
 		for (tmp = arr[i]; leftChild(i) < n; i = child)
 		{
 			child = leftChild(i);
-			if (child != n - 1 && arr[child] < arr[child + 1]) // the condition "child != n - 1" is important
+			// the condition "child != n - 1" is important
+			if (child != n - 1 && arr[child] < arr[child + 1])
 			{
 				child++;
 			}
@@ -52,7 +56,6 @@ public class HeapSort
 			}
 		}
 		arr[i] = tmp;
-
 	}
 
 	public static void swap(int[] arr, int i ,int j)
